83d257886c00ed912c1380e5fd69eecd1d7d589f
bstraehle <bstraehle@gmail.com>
2022-01-28 15:10:17 -0800

    Logging

--- Controllers/DemoController.cs
+++ Controllers/DemoController.cs
@@ -13,11 +13,15 @@ namespace DemoApp.Controllers
         public DemoController(IConfiguration config)
         {
             demoAPIhost = config.GetValue("DemoAPIhost", "");
-            demoAPIapp = config.GetValue("DemoAPIapp", ""); ;
+            demoAPIapp = config.GetValue("DemoAPIapp", "");
+
+            Console.Out.WriteLineAsync(string.Format("URI=[{0}{1}]", demoAPIhost, demoAPIapp));
         }
 
         public async Task<IActionResult> List()
         {
+            await Console.Out.WriteLineAsync("List");
+
             var client = new HttpClient();
             var response = await client.GetAsync(new Uri(demoAPIhost + demoAPIapp)).ConfigureAwait(false);
             response.EnsureSuccessStatusCode();
@@ -28,27 +32,31 @@ namespace DemoApp.Controllers
 
         public async Task<IActionResult> View(int id)
         {
-            return View(await GetDemoAsync(id));
+            return View(await GetAsync(id));
         }
 
         public async Task<IActionResult> Update(int id)
         {
-            return View(await GetDemoAsync(id));
+            return View(await GetAsync(id));
         }
 
         public async Task<IActionResult> Delete(int id)
         {
-            return View(await GetDemoAsync(id));
+            return View(await GetAsync(id));
         }
 
         public IActionResult Add()
         {
+            Console.Out.WriteLineAsync("Add");
+
             return View(new Demo());
         }
 
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> UpdateObject(Demo demo)
         {
+            await Console.Out.WriteLineAsync(string.Format("UpdateObject {0}", demo));
+
             var client = new HttpClient();
             var content = new StringContent(JsonConvert.SerializeObject(demo), Encoding.UTF8, "application/json");
             var response = await client.PutAsync(new Uri(demoAPIhost + demoAPIapp + "/" + demo.Id), content).ConfigureAwait(false);
@@ -59,6 +67,8 @@ namespace DemoApp.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteObject(Demo demo)
         {
+            await Console.Out.WriteLineAsync(string.Format("DeleteObject {0}", demo));
+
             var client = new HttpClient();
             var response = await client.DeleteAsync(new Uri(demoAPIhost + demoAPIapp + "/" + demo.Id)).ConfigureAwait(false);
             response.EnsureSuccessStatusCode();
@@ -68,6 +78,8 @@ namespace DemoApp.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddObject(Demo demo)
         {
+            await Console.Out.WriteLineAsync(string.Format("AddObject {0}", demo));
+
             var client = new HttpClient();
             var content = new StringContent(JsonConvert.SerializeObject(demo), Encoding.UTF8, "application/json");
             var response = await client.PostAsync(new Uri(demoAPIhost + demoAPIapp), content).ConfigureAwait(false);
@@ -75,8 +87,10 @@ namespace DemoApp.Controllers
             return RedirectToAction("List");
         }
 
-        private async Task<Demo> GetDemoAsync(int id)
+        private async Task<Demo> GetAsync(int id)
         {
+            await Console.Out.WriteLineAsync(string.Format("Get Id=[{0}]", id));
+
             var client = new HttpClient();
             var response = await client.GetAsync(new Uri(demoAPIhost + demoAPIapp + "/" + id)).ConfigureAwait(false);
             response.EnsureSuccessStatusCode();
--- Models/Demo.cs
+++ Models/Demo.cs
@@ -6,5 +6,10 @@ namespace DemoApp.Models
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Id=[{0}], Name=[{1}]", Id, Name);
+        }
     }
 }
